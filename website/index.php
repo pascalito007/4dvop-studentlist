daaa639684315191658a155c832301e00e49e80f
salah <salah.bellagnaoui@gmail.com>
2020-04-06 14:42:05 +0100

    same changes

--- website/index.php
+++ website/index.php
@@ -29,7 +29,7 @@
 
 
 
-              $url = 'http://localhost:5001/pozos/api/v1.0/get_student_ages';
+              $url = 'http://api:5000/pozos/api/v1.0/get_student_ages';
 
               $list = json_decode(file_get_contents($url, false, $context), true);
               echo "<p style='color:red;; font-size: 20px;'>This is the list of the student with age</p>";
