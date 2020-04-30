16238a6305787cceb43f394f47782a76316c3544
MEWENEMESSE Jean-Pascal <pasciano007@gmail.com>
2020-04-30 11:27:58 +0200

    furfilling dockerfile and compose

--- website/index.php
+++ website/index.php
@@ -8,9 +8,9 @@
         <h1>Student Checking App</h1>
         <ul>
             <form action="" method="POST">
-            <!--<label>Enter student name:</label><br />
+            <label>Enter student name:</label><br />
             <input type="text" name="" placeholder="Student Name" required/>
-            <br /><br />-->
+            <br /><br />
             <button type="submit" name="submit">List Student</button>
             </form>
 
